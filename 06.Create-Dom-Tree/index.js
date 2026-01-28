c571e77abcc1fe37014a6c8a768cd69801e15791
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-28 20:40:04 +0700

    update create dom tree

---
+++ 06.Create-Dom-Tree/index.js
@@ -0,0 +1,22 @@
+const root = document.createElement("html");
+root.lang = "en";
+
+const head = document.createElement("head");
+const title = document.createElement("title");
+
+title.appendChild(document.createTextNode("Document"));
+head.appendChild(title);
+
+const body = document.createElement("body");
+const header = document.createElement("header");
+header.appendChild(document.createTextNode("Header"));
+
+body.appendChild(header);
+
+const paragraph = document.createElement("paragraph");
+paragraph.appendChild(document.createTextNode("Paragraph"));
+
+body.appendChild(paragraph);
+
+html.appendChild(body);
+html.appendChild(head);
